03cf72765e245db4b316d438881473d9fed0977c
Shamima Sultana <shamima.milee@gmail.com>
2025-06-18 16:13:09 +0600

    Updated requisition approve list page and happypath

--- tests/test_happy_path_1.py
+++ tests/test_happy_path_1.py
@@ -9,6 +9,7 @@ from pages.login_page import LoginPage
 from pages.procurement_home_page import ProcurementHomePage
 from resources.resource_file import TestResources
 from playwright.sync_api import sync_playwright
+from pages.requisition_approve_list import RequisitionApproveList   
 
 install()
 
@@ -38,35 +39,44 @@ def test_two(resource):
     d_page.goto_procurement()
     #d_page.get_screen_shot('modular_test_2')
 
-def test_three(resource):
-    p_page = ProcurementHomePage(resource)
-    p_page.navigate_to_create_requisition()
-    #p_page.get_screen_shot('modular_test_3')
+# def test_three(resource):
+#     p_page = ProcurementHomePage(resource)
+#     p_page.navigate_to_create_requisition()
+#     #p_page.get_screen_shot('modular_test_3')
 
-def test_four(resource):
-    c_page = CreateRequisitionPage(resource)
-    try:
-        c_page.verify_by_title("Create Requisition")
-        #c_page.get_screen_shot('modular_test_4')
-        c_page.select_requisition_for_self()
-        c_page.input_project_name("[H04] - Procurement-BRAC")
-        c_page.input_source_of_fund("BRAC")
-        c_page.input_item_information("[19190]-Glue-(Supplies and Stationeries->Supplies and Stationeries->Stationery)", "BAG")
-        c_page.wait_for_timeout(10000)
-        c_page.input_item_quantity("10")
-        c_page.input_item_unit_price("100")
-        c_page.set_delivery_schedule()
-        c_page.set_requisition_details()        # Providing GL details also
-        c_page.wait_for_timeout(5000)
-        c_page.get_full_page_screenshot('modular_test_5')
-        c_page.add_item_to_grid()
-        c_page.get_full_page_screenshot('modular_test_6')
-        c_page.save_requisition()
-        #c_page.confirm_submit_requisition()
-        c_page.get_full_page_screenshot('modular_test_7')
-        c_page.wait_for_timeout(5000)
-        c_page.track_requisition_number()
-        c_page.get_full_page_screenshot('modular_test_8')
-    except Exception as e:
-        c_page.get_full_page_screenshot('test_four_error')
-        raise e
+# def test_four(resource):
+#     c_page = CreateRequisitionPage(resource)
+#     try:
+#         c_page.verify_by_title("Create Requisition")
+#         #c_page.get_screen_shot('modular_test_4')
+#         c_page.select_requisition_for_self()
+#         c_page.input_project_name("[H04] - Procurement-BRAC")
+#         c_page.input_source_of_fund("BRAC")
+#         c_page.input_item_information("[19190]-Glue-(Supplies and Stationeries->Supplies and Stationeries->Stationery)", "BAG")
+#         c_page.wait_for_timeout(10000)
+#         c_page.input_item_quantity("10")
+#         c_page.input_item_unit_price("100")
+#         c_page.set_delivery_schedule()
+#         c_page.set_requisition_details()        # Providing GL details also
+#         c_page.wait_for_timeout(5000)
+#         c_page.get_full_page_screenshot('modular_test_5')
+#         c_page.add_item_to_grid()
+#         c_page.get_full_page_screenshot('modular_test_6')
+#         c_page.save_requisition()
+#         #c_page.confirm_submit_requisition()
+#         c_page.get_full_page_screenshot('modular_test_7')
+#         c_page.wait_for_timeout(5000)
+#         c_page.track_requisition_number()
+#         c_page.get_full_page_screenshot('modular_test_8')
+#     except Exception as e:
+#         c_page.get_full_page_screenshot('test_four_error')
+#         raise e
+def test_five(resource):
+    r_page = RequisitionApproveList(resource)
+    r_page.navigate_to_url("https://env28.erp.bracits.net/procurementDashboard/myDashboard#!/requisition/list")
+
+    r_page.search_requisition(r_page.requisition_number)
+    r_page.select_requisition()
+    r_page.approve_requisition()
+    r_page.get_screen_shot('modular_test_9')
+    #r_page.get_full_page_screenshot('modular_test_10')
