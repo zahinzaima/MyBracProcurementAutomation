e7b319645f0567ae22940a61338297b205ecb9c8
raajputro <mahbuburrahmannajib@gmail.com>
2025-06-19 17:53:40 +0600

    refined test four

--- pages/create_requisition_page.py
+++ pages/create_requisition_page.py
@@ -12,9 +12,13 @@ class CreateRequisitionPage(ProcurementHomePage, BasicActions):
         # write down all the elements here with locator format
         self.proc_item_requisition_for_self = page.locator('input[type="radio"][id="self"]')
         self.proc_item_requisition_for_other = page.locator('input[type="radio"][id="other"]')
+        self.proc_item_requisition_for_other_office = page.locator("#officeInfoDiv_arrow")
+
         self.proc_item_project = page.locator('css=#projectInfoDiv_input')
         self.proc_item_project_dropdown = page.locator('#projectInfoDiv_arrow')
         self.proc_item_source_of_fund = page.locator('css=#sourceOfFundDiv_arrow')
+        self.proc_item_remarks = page.get_by_role("textbox", name="Max size of requisition remarks 300 characters")
+
         self.proc_item_information = page.locator('css=#itemInfo')
         self.proc_item_unit_measure = page.locator('css=#mUnitDiv_arrow')
 
@@ -41,8 +45,9 @@ class CreateRequisitionPage(ProcurementHomePage, BasicActions):
         self.submit_button = page.get_by_role("button", name=re.compile("submit", re.IGNORECASE))
         self.confirm_submit_button = page.locator('css=button[role="button"] span[class="ui-button-text"]')
         # requisition number elements
-        self.requisition_message = page.locator('//div[@class="message"]')
-        self.requisition_message_2 = page.locator('//div[@class="message"]')
+        # self.requisition_message = page.locator('//div[@class="message"]')
+        # self.requisition_message_2 = page.locator('//div[@class="message"]')
+        self.requisition_message = page.get_by_text("Procurement Requisition", exact=True)
 
         # jGrowl > div.jGrowl-notification.ui-state-highlight.ui-corner-all.success
         # jGrowl > div.jGrowl-notification.ui-state-highlight.ui-corner-all.success > div.message
@@ -54,19 +59,22 @@ class CreateRequisitionPage(ProcurementHomePage, BasicActions):
 
 
     # methods to perform actions within the page
-    def select_requisition_for_self(self):
-        self.proc_item_requisition_for_self.click()
-
-    def select_requisition_for_other(self):
-        self.proc_item_requisition_for_other.click()
-
-    def input_project_name(self, project_name):
+    def set_requisition_for_HO_and_project(self, ho, project_name):
+        if ho:
+            self.proc_item_requisition_for_self.click()
+        else:
+            self.proc_item_requisition_for_other.click()
+            self.proc_item_requisition_for_other_office.click()
+            # Click on Chattagram(D00014) branch
+            self.page.get_by_text("171798692561Chattagram(DO0014)").click()
         self.select_from_dropdown(self.proc_item_project_dropdown, project_name)
 
-    def input_source_of_fund(self, source_of_fund):
+    def set_requisition_information(self, source_of_fund, remarks):
         self.select_from_dropdown(self.proc_item_source_of_fund, source_of_fund)
+        self.input_in_element(self.proc_item_remarks, remarks)
 
-    def input_item_information(self, item_information, item_measure_unit):
+
+    def set_requisition_details(self, item_information, item_measure_unit, quantity, unit_price):
         self.input_in_element(self.proc_item_information, item_information)
         self.press_button("Enter")
         #expect(self.proc_item_unit_measure).to_be_visible()
@@ -74,26 +82,20 @@ class CreateRequisitionPage(ProcurementHomePage, BasicActions):
         #self.press_button("Enter")
         self.input_in_element(self.proc_item_tor, 'More Glue')
         # expect(self.proc_item_tor).to_have_text('Glue (any type of glue)')
-
-    def input_item_quantity(self, quantity):
         self.input_in_element(self.proc_item_quantity, quantity)
         # if self.requisition_cost_quantity.is_checked():
         #     expect(self.requisition_item_qty_amount).to_have_text(quantity)
-
-    def input_item_unit_price(self, unit_price):
         self.input_in_element(self.proc_item_unit_price, unit_price)
         # if self.requisition_cost_amount.is_checked():
         #     qty = self.proc_item_quantity.get_attribute("value")
         #     expect(self.requisition_item_qty_amount).to_have_value(qty*unit_price)
 
-    def set_delivery_schedule(self):
-        self.click_on_btn(self.requisition_delivery_schedule)
-        self.input_in_element(self.requisition_delivery_date, "05-06-2025")
-        self.select_from_list_by_value(self.requisition_delivery_location, "Central Store")
-
-    def set_requisition_details(self):
+    def set_requisition_project_delivery_schedule(self, gl_code, delivery_date, delivery_location):
         self.requisition_for_single_project.click()
-        self.select_from_dropdown(self.requisition_item_gl_code, "[5102010107-05] Remuneration")
+        self.select_from_dropdown(self.requisition_item_gl_code, gl_code)
+        self.click_on_btn(self.requisition_delivery_schedule)
+        self.input_in_element(self.requisition_delivery_date, delivery_date)
+        self.select_from_list_by_value(self.requisition_delivery_location, delivery_location)
 
     def add_item_to_grid(self):
         self.click_on_btn(self.requisition_add_button)
@@ -102,15 +104,15 @@ class CreateRequisitionPage(ProcurementHomePage, BasicActions):
     def save_requisition(self):
         self.click_on_btn(self.save_button)
 
-    def submit_requisition(self):
-        self.click_on_btn(self.submit_button)
-        self.page.wait_for_timeout(10000)
-
-    def confirm_submit_requisition(self):
-        self.wait_to_load_element(self.confirm_submit_button)
-        self.click_on_btn(self.confirm_submit_button)
+    # def submit_requisition(self):
+    #     self.click_on_btn(self.submit_button)
+    #     self.page.wait_for_timeout(10000)
+    #
+    # def confirm_submit_requisition(self):
+    #     self.wait_to_load_element(self.confirm_submit_button)
+    #     self.click_on_btn(self.confirm_submit_button)
 
     def track_requisition_number(self):
         #self.wait_to_load_element(self.requisition_message)
-        val_text = self.requisition_message_2.text_content()
+        val_text = self.requisition_message.text_content()
         print("Requisition Info: " + val_text)
--- tests/test_happy_path_1.py
+++ tests/test_happy_path_1.py
@@ -35,23 +35,32 @@ def test_four(resource):
         c_page.navigate_to_url("https://env28.erp.bracits.net/procurementDashboard/myDashboard#!/requisition/create")
         c_page.verify_by_title("Create Requisition")
         c_page.get_screen_shot('modular_test_4')
-        c_page.select_requisition_for_self()
-        c_page.input_project_name("[H04] - Procurement-BRAC")
-        c_page.input_source_of_fund("BRAC")
-        c_page.input_item_information("[19190]-Glue-(Supplies and Stationeries->Supplies and Stationeries->Stationery)", "BAG")
+        c_page.set_requisition_for_HO_and_project(
+            True,
+            "[H04] - Procurement-BRAC")
+        c_page.set_requisition_information(
+            "112BRAC",
+            "requisition remarks")
+        c_page.set_requisition_details(
+            "[19190]-Glue-(Supplies and Stationeries->Supplies and Stationeries->Stationery)",
+            "BAG",
+            "10",
+            "150")
         c_page.wait_for_timeout(10000)
-        c_page.input_item_quantity("10")
-        c_page.input_item_unit_price("100")
-        c_page.set_delivery_schedule()
-        c_page.set_requisition_details()        # Providing GL details also
+        c_page.set_requisition_project_delivery_schedule(
+            "[5102010107-05] Remuneration",
+            "05-06-2025",
+            "Central Store")
         c_page.wait_for_timeout(5000)
         c_page.get_full_page_screenshot('modular_test_5')
+
         c_page.add_item_to_grid()
         c_page.get_full_page_screenshot('modular_test_6')
+
         c_page.save_requisition()
-        #c_page.confirm_submit_requisition()
         c_page.get_full_page_screenshot('modular_test_7')
         c_page.wait_for_timeout(5000)
+
         c_page.track_requisition_number()
         c_page.get_full_page_screenshot('modular_test_8')
     except Exception as e:
