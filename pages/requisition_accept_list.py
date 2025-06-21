541f4e1da7443ed63dcb31f35ab9b19e40b40e6c
Shamima Sultana <shamima.milee@gmail.com>
2025-06-21 21:03:52 +0600

    updated requisition accptlist and search is now working

--- pages/requisition_accept_list.py
+++ pages/requisition_accept_list.py
@@ -13,14 +13,17 @@ class RequisitionAcceptList(ProcurementHomePage, BasicActions):
         self.select_all = page.get_by_role("link", name="Select All", exact=True)
         self.approve = page.get_by_role("button", name=re.compile("Approve", re.IGNORECASE))
         self.confirmation_message_approve = page.get_by_role("button", name=re.compile("Approve", re.IGNORECASE))
-    
+        
+
     def search_requisition(self, requisition_number):
-        self.input_in_element(self.req_no, requisition_number)
-        self.page.wait_for_timeout(4000)
-        self.page.keyboard.press("Enter")
-        self.page.keyboard.press("Enter")
+        self.input_in_element(self.req_no, requisition_number + " ") # adding a trailing space to ensure the input is recognized
+        self.page.wait_for_timeout(1000)
+        self.req_no.click()  # focus the input field
+        self.page.keyboard.press("End")  # move cursor to the end
+        self.page.keyboard.press("Backspace")  # delete trailing space
+        
         self.page.wait_for_timeout(2000)
-        self.find.click()
+        # self.find.click()
         self.page.wait_for_timeout(4000)
         #self.page.wait_for_timeout(2000)
         # self.select_all.click()
