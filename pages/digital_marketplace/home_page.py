298e48117eae004a4e5ae18d2a2c4a78318b44ab
fahmida.akther <fahmida.himu0904@gmail.com>
2025-07-14 21:50:19 +0600

    test_login_page,home_page,main_navigation_menu pages commit and push

--- pages/digital_marketplace/home_page.py
+++ pages/digital_marketplace/home_page.py
@@ -4,3 +4,22 @@ from utils.basic_actionsdm import BasicActionsDM
 class HomePage(BasicActionsDM):
     def __init__(self, page):
         super().__init__(page)
+        # self    = page
+        # write down all the elements here with locator format
+        # self.myDashboardItem_HRM = page.locator('xpath=//*[contains(text(),"HRM")]')
+        self.userHomePageItem_ShowFrameworkAgreementList = page.locator(
+            'xpath=//*[contains(text(),"All Framework Agreements")]')
+        self.userHomePageItem_ShowAllVendors = page.locator('xpath=//*[contains(text(),"All Vendors ")]')
+        self.userHomePageItem_showAllCategories = page.locator('xpath=//*[contains(text(),"All Categories")]')
+
+        # Locator: find the span with class "cart-label" and text "Shopping cart"
+        self.shopping_cart1 = page.locator('a:has-text("Shopping cart")')
+        # self.shopping_cart = page.locator('span.cart-label', has_text='Shopping cart')
+        # self.userHomePageItem_showShoppingCartList = page.locator('/html/body/div[6]/div[2]/div[1]/div[2]/div[1]/ul/a/span[1]')
+
+    def click_shopping_cart(self):
+        # self.shopping_cart1.click()
+        self.click_on_btn(self.shopping_cart1)
+        # def goto_shoppingcart(self):
+        #     (self.click_on_btn(self.userHomePageItem_showShoppingCartList)
+        # userHomePageItem_showShoppingCartList.click())
--- pages/digital_marketplace/main_navigation_menu.py
+++ pages/digital_marketplace/main_navigation_menu.py
@@ -1,9 +1,18 @@
-class MainNavigationMenu():
-    def __init__(self):
-        self.page = None
-        self.menu_item_manage = self.page.locator("a#btn_login.btn_user")
+from utils.basic_actionsdm import BasicActionsDM
 
 
-# class MainNavigationMenu():
-#     def __init__(self):
-#         self.menu_item_manage = self.page.locator('css=#manage_menu')
+class MainNavigationMenu(BasicActionsDM):
+    def __init__(self, page):
+        super().__init__(page)
+        # write down all the elements here with locator format
+        self.exit_button = page.locator("a#ico-logout./logout")
+
+        self.logout_button = self.page.locator('a:has-text("Log out")')
+        self.first_common_login_btn = page.locator("//button[@type='button']")
+
+    def perform_logout(self, logout_button):
+        self.click_on_btn(self.logout_button)
+        # self.logout_button.click()
+        # self.exit_button.click()
+        self.wait_for_timeout(1000)
+        # self.logout_button.click()
